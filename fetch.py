c4d7bffd1262dc1b5726758ae0d1fa0915a3f1ea
xiaojay <xiaojay@gmail.com>
2012-06-05 00:34:46 +0800

    impove fetch

--- fetch.py
+++ fetch.py
@@ -1,7 +1,12 @@
 #coding=utf-8
-import time
+import time, logging
 from person import parse
+logfile = 'fetch.log'
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - - %(asctime)s %(message)s', 
+                    datefmt='[%d/%b/%Y %H:%M:%S]', filename=logfile)
+
 ids = open('ids.txt').read().split('\n')[:-1]
+fetched = 0
 for id_ in ids:
     print id_
     info = parse(id_)
@@ -9,4 +14,10 @@ for id_ in ids:
         if i: i = i.encode('u8')
         print i
     print
+    
+    fetched += 1
+    if fetched%100 == 0:
+        logging.info('fetched %i.'%fetched)
+        logging.info('fetched id_ %s just now.'%id_)
     time.sleep(0.2)
+    
--- person.py
+++ person.py
@@ -1,10 +1,21 @@
 #coding=utf-8
-import re, urllib
+import re, urllib2, socket
 from BeautifulSoup import BeautifulSoup
 def parse(id_):
+    tried_times = 0 
     url = 'http://www.npc.gov.cn/delegate/viewDelegate.action?dbid=%s'%id_
-    html = urllib.urlopen(url).read()
-
+    while tried_times < 5:
+        try:
+            html = urllib2.urlopen(url, timeout=3).read()
+            break
+        except urllib2.URLError:
+            tried_times += 1
+        except socket.timeout:
+            tried_times += 1
+            
+    if tried_times >= 5:
+        return ['error']
+     
     b = BeautifulSoup(html)
     tb = b.find('table', attrs={'class':'table_text'})
     trs = tb.findAll('tr')
